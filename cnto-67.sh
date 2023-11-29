fd75e0c046b0631122ec7238c0f7f7978009b304
smolix01 <50324713+smolix01@users.noreply.github.com>
2023-11-29 12:58:41 +0100

    Update cnto-67.sh

--- cnto-67.sh
+++ cnto-67.sh
@@ -1,88 +1,75 @@
 #!/bin/bash
 
 # Prompt the user to enter the new domain name
-read -p "Enter the new domain name: " new_domain_name
+read -p "Enter the new domain name: " DOMAIN
+read -p "Enter SELECTOR: " SELECTOR
+read -p "Enter your email address (for Let's Encrypt): " EMAIL_ADDRESS
 
 # Set the new domain name as the hostname
-sudo hostname $new_domain_name
-sudo sed -i "s/HOSTNAME=.*/HOSTNAME=$new_domain_name/g" /etc/sysconfig/network
+sudo hostname $DOMAIN
+sudo sed -i "s/HOSTNAME=.*/HOSTNAME=$DOMAIN/g" /etc/sysconfig/network
 sudo service network restart
 
 # Display a message confirming the change
-echo "Hostname has been set to: $new_domain_name"
-
-# Remove existing PHP packages (if any)
-sudo yum remove php*
-
-# Add the Webtatic repository for PHP 5.6 (CentOS 6)
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
-
-# Add the Webtatic repository for PHP 5.6 (CentOS 7)
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/latest.rpm
+echo "Hostname has been set to: $DOMAIN"
 
 # Install required packages
-sudo yum install epel-release
-sudo yum install php56w php56w-opcache php56w-mysql php56w-mbstring php56w-mcrypt php56w-devel php56w-xml php56w-json php56w-fpm php56w-gd
+sudo yum install -y epel-release
+sudo yum install -y httpd php56w php56w-opcache php56w-mysql php56w-mbstring php56w-mcrypt php56w-devel php56w-xml php56w-json php56w-fpm php56w-gd
 
-# Install Apache (httpd)
-sudo yum install httpd
+# Start and enable the Apache HTTP server
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
 # Install Postfix
-sudo yum install postfix
+sudo yum install -y postfix
 sudo systemctl start postfix
 sudo systemctl enable postfix
 
-# Install OpenDKIM
-sudo yum install opendkim opendkim-tools
-sudo systemctl start opendkim
-sudo systemctl enable opendkim
+# Install Dovecot (for IMAP and POP3)
+sudo yum install -y dovecot
 
-# Generate a DKIM key pair
-sudo opendkim-genkey -t -s mail -d $new_domain_name
-sudo mv mail.private /etc/opendkim/keys/$new_domain_name.private
-sudo chown opendkim:opendkim /etc/opendkim/keys/$new_domain_name.private
+# Install OpenDKIM
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/latest.rpm
+sudo yum install -y opendkim opendkim-tools
+
+# Generate DKIM keys
+sudo opendkim-genkey -b 2048 -h rsa-sha256 -r -s "$SELECTOR" -d "$DOMAIN"
 
 # Configure OpenDKIM
-sudo echo "KeyTable /etc/opendkim/key.table" >> /etc/opendkim.conf
-sudo echo "SigningTable refile:/etc/opendkim/signing.table" >> /etc/opendkim.conf
-sudo echo "InternalHosts /etc/opendkim/trusted.hosts" >> /etc/opendkim.conf
+sudo mkdir -p /etc/opendkim/keys/${DOMAIN}
+sudo mv "${SELECTOR}.private" "/etc/opendkim/keys/${DOMAIN}/${SELECTOR}.private"
+sudo chown opendkim:opendkim "/etc/opendkim/keys/${DOMAIN}/${SELECTOR}.private"
+sudo chmod 400 "/etc/opendkim/keys/${DOMAIN}/${SELECTOR}.private"
 
-# Add your domain and DKIM key to the configuration files
-sudo echo "mail._domainkey.$new_domain_name $new_domain_name:mail:/etc/opendkim/keys/$new_domain_name.private" >> /etc/opendkim/key.table
-sudo echo "*@$new_domain_name mail._domainkey.$new_domain_name" >> /etc/opendkim/signing.table
-sudo echo "$new_domain_name" >> /etc/opendkim/trusted.hosts
+# Add the DKIM configuration to OpenDKIM configuration file
+echo "Domain *.$DOMAIN" | sudo tee -a /etc/opendkim.conf
+echo "KeyFile /etc/opendkim/keys/${DOMAIN}/${SELECTOR}.private" | sudo tee -a /etc/opendkim.conf
+echo "Selector ${SELECTOR}" | sudo tee -a /etc/opendkim.conf
+echo "SOCKET=\"inet:8891@localhost\"" | sudo tee -a /etc/opendkim.conf
 
-# Restart OpenDKIM
+# Update Postfix configuration to use OpenDKIM
+sudo postconf -e "smtpd_milters = inet:localhost:8891"
+sudo postconf -e "non_smtpd_milters = inet:localhost:8891"
+sudo postconf -e "milter_default_action = accept"
+
+# Install Certbot (Let's Encrypt client)
+sudo yum install -y certbot python2-certbot-apache
+
+# Obtain and install SSL certificate for your domain
+sudo certbot --apache -d $DOMAIN -m $EMAIL_ADDRESS --agree-tos --no-eff-email
+
+# Create a welcome page
+sudo mkdir -p /var/www/html
+echo "<html><body><h1>Welcome to my website!</h1></body></html>" | sudo tee /var/www/html/index.html
+
+# Restart services
 sudo systemctl restart opendkim
+sudo systemctl restart postfix
+sudo systemctl restart httpd
+sudo systemctl restart dovecot
 
-# Configure Postfix for sender canonical mapping and header checks
-sudo echo "sender_canonical_maps = hash:/etc/postfix/canonical" >> /etc/postfix/main.cf
-sudo echo "mime_header_checks = regexp:/etc/postfix/header_checks" >> /etc/postfix/main.cf
-sudo echo "header_checks = regexp:/etc/postfix/header_checks" >> /etc/postfix/main.cf
+echo "DKIM and SSL setup completed for $DOMAIN."
 
-# Create header checks file
-sudo cat > /etc/postfix/header_checks << EOF
-/^Received:.*\(Postfix/ IGNORE
-EOF
-
-# Create canonical file
-sudo cat > /etc/postfix/canonical << EOF
-www-data  noreply
-EOF
-
-# Postmap the canonical file
-sudo postmap /etc/postfix/canonical
-
-# Install additional packages (unzip in this case)
-sudo yum install unzip
-
-# Additional steps or configurations can be added here
-
-# Change to the web directory
-cd /var/www/html
-
-# You can add more commands or configurations as needed for your specific setup
-
-echo "Script execution completed."
+# Install additional packages
+sudo yum install -y unzip
